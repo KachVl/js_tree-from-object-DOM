4768897e748f4eb7cb51fea4a34f7247cc7d5270
Vlad Kach <kachevl@gmail.com>
2024-05-29 14:44:53 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,36 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTreeDom(obj) {
+    if (!Object.keys(obj).length) return;
+    const ul = document.createElement("ul");
+    for(const key in obj){
+        const li = document.createElement("li");
+        li.innerHTML = key;
+        const childrenUl = createTreeDom(obj[key]);
+        if (childrenUl) li.append(childrenUl);
+        ul.append(li);
+    }
+    return ul;
+}
+function createTree(element, data) {
+    element.append(createTreeDom(data));
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
